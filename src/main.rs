cf91d9c55f197d8353587372fa1683701ae43916
Mike Cronce <mike@cronce.io>
2020-02-23 18:41:12 -0500

    src/main.rs:  + /services.list route and handler

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,48 @@ struct State {
 	client: kube::client::APIClient
 }
 
+#[responder]
+async fn services(state: actix_web::web::Data<State>) -> actix_helper_macros::ResponderResult<()> {
+	// STARS, STARS everywhere.  Make it rain STARS.
+	let services = kube::api::Api::v1Service((**state).client.clone());
+
+	let default_services = services.clone().within("default").list(&kube::api::ListParams::default()).await?;
+	let all_services = services.list(&kube::api::ListParams::default()).await?;
+
+	let mut lines = Vec::with_capacity(default_services.items.len() + all_services.items.len());
+
+	for service in default_services.iter() {
+		let cluster_ip = match &service.spec.cluster_ip {
+			None => continue,
+			Some(s) => s
+		};
+		if(cluster_ip == "None") {
+			continue;
+		}
+		lines.push(format!("{} {}{}", cluster_ip, service.metadata.name, state.tld));
+	}
+
+	for service in all_services.iter() {
+		let cluster_ip = match &service.spec.cluster_ip {
+			None => continue,
+			Some(s) => s
+		};
+		if(cluster_ip == "None") {
+			continue;
+		}
+		let mut namespace = match &service.metadata.namespace {
+			None => "default",
+			Some(s) => s
+		};
+		if(namespace == "") {
+			namespace = "default";
+		}
+		lines.push(format!("{} {}.{}{}", cluster_ip, service.metadata.name, namespace, state.tld));
+	}
+
+	Ok(actix_helper_macros::text!(lines.join("\n") + "\n"))
+}
+
 #[actix_rt::main]
 async fn main() -> Result<(), Box<dyn Error>> {
 	let port = match env::var_os("NAMER_PORT") {
@@ -56,6 +98,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
 	let result = actix_web::HttpServer::new(move || actix_web::App::new()
 		.data(state.clone())
 		.wrap(actix_web::middleware::Logger::default())
+		.route("/services.list", actix_web::web::get().to(services))
 		.service(actix_files::Files::new("/", &dir))
 	).bind(format!("0.0.0.0:{}", port))?.run().await?;
 	Ok(result)
